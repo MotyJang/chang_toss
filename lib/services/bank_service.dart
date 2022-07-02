01b600cdfe19c9b780bfe01f80910a1e4cd6d6b4
seokjin <7271kim@naver.com>
2022-07-02 16:28:30 +0900

    send page 개발 완료

--- lib/services/bank_service.dart
+++ lib/services/bank_service.dart
@@ -1,9 +1,57 @@
+import 'dart:convert';
+
 import 'package:chang_mini/model/bank.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BankService extends ChangeNotifier {
   var f = NumberFormat('###,###,###,###');
 
-  List<Bank> myBankList = [];
+  SharedPreferences? savedData;
+  List<Bank> myBankList = [new Bank(name: 'a', bankImage: 'aaa', balance: 100)];
+
+  BankService(SharedPreferences this.savedData) {
+    getPrefsData();
+  }
+
+  createdBank(Bank bank) async {
+    myBankList.add(bank);
+    await saveData();
+
+    notifyListeners();
+  }
+
+  bool sendMoney(Bank fromBank, Bank toBank, int money) {
+    if ((fromBank.balance ?? 0) < money) {
+      return false;
+    }
+
+    fromBank.balance = (fromBank.balance ?? 0) - money;
+    toBank.balance = (toBank.balance ?? 0) + money;
+
+    saveData();
+
+    notifyListeners();
+    return true;
+  }
+
+  getPrefsData() {
+    String? savedString = savedData?.getString('banks');
+    if (savedString != null) {
+      myBankList = [];
+      jsonDecode(savedString).forEach((e) {
+        myBankList.add(Bank.fromJson(e));
+      });
+    }
+  }
+
+  saveData() {
+    savedData?.setString(
+      'banks',
+      jsonEncode(
+        myBankList.map((e) => e.toJson()).toList(),
+      ),
+    );
+  }
 }
